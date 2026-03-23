2466edb8185ab69d121aeb1597b3f4f8040eec8b
ALI AHMED <prgmr.aahmed@gmail.com>
2026-03-24 00:24:56 +0500

    Implement Observer pattern with User and Course classes

--- LMS.cpp
+++ LMS.cpp
@@ -3,7 +3,14 @@
 #include <vector>
 using namespace std;
 
-class User{
+
+class IObserver {
+public:
+    virtual void update(string message) = 0; // Receives notifications
+    virtual ~IObserver() {}
+};
+
+class User : public IObserver{
 protected:
 	string name;
 	const int id;
@@ -13,6 +20,9 @@ public:
 		name = n;
 		totalUsers++;
 	}
+	void update(string message) override {
+        cout << "Notification to " << name << " (ID: " << id << "): " << message << endl;
+    }
 	virtual void displayDashboard() = 0;
 	static int getTotalUsers(){ return totalUsers; }
 	virtual ~User() {}
@@ -28,31 +38,16 @@ public:
 	}
 	
 	void displayDashboard() override {
+		cout << "==== Student Portal ====" << endl;
 		cout<<"Name: "<<name<<"\nID: "<<id<<"\nCGPA: "<<cgpa<<endl;
 	}
 	
 	void setCGPA(float n){
-		if(n>0.0 && n<4.0)
+		if(n >= 0.0 && n <= 4.0)
 		cgpa = n;
 	}
 	string getName(){return name;}
-};
-
-class Teacher: public User{
-	string subject;
-public:
-	Teacher(string n,int i,string s):User(n,i){
-		subject = s;
-		cout<<"You are successfully registered as a teacher"<<endl;
-	}
 	
-	void displayDashboard() override {
-		cout<<"Name: "<<name<<"\nID: "<<id<<"\nSubject: "<<subject<<endl;
-	}
-	 
-	void setSubject(string s){
-		subject = s;
-	}
 };
 
 class Course{
@@ -65,5 +60,75 @@ public:
 		studentList.push_back(s);
 		cout<<s->getName()<<" added to "<<courseName<<endl;
 	}
+	
+	string getCourseName(){ return courseName;	}
+	void displayStudentList(){
+		for (Student* i: studentList){
+			if(i)
+			cout<<i->getName()<<endl;
+		}
+	}
+	
+	void postAnnouncement(string msg){
+		cout<<"New update in "<<courseName<<endl;
+		for (Student* s: studentList){
+			s->update(msg);
+		}
+	}
 };
 
+class Teacher: public User{
+	string subject;
+	vector<Course*> managedCourses;
+public:
+	Teacher(string n,int i,string s):User(n,i){
+		subject = s;
+		cout<<"You are successfully registered as a teacher"<<endl;
+	}
+	
+	void addCourse(Course* c){
+		managedCourses.push_back(c);
+	}
+	
+	vector<Course*> getCourses() {
+        return managedCourses;
+    }
+	
+	void displayDashboard() override {
+		cout << "==== Teacher Portal ====" << endl;
+        cout << "Teacher: " << name << " | Subject: " << subject << endl;
+        cout << "Currently managing " << managedCourses.size() << " class(es)." << endl;
+        for (Course* i : managedCourses){
+        	if(i)
+        	cout<<i->getCourseName()<<endl;
+		}
+		cout << "=========================" << endl;
+	}
+	
+	void announce(Course* c, string message){
+		c->postAnnouncement(message);
+	}
+	
+	void setSubject(string s){
+		subject = s;
+	}
+};
+
+
+
+class UserFactory {
+	static 
+};
+
+int main(){
+	Teacher t1("Ali",986,"Maths");
+	Student s1("ahmed",326,3);
+	Course c1("MATHS");
+	
+	t1.addCourse(&c1);
+	
+	t1.displayDashboard();
+	c1 += &s1;
+	
+    
+}
