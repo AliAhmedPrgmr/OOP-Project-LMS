3bbcb554f3d815aa74208b64d8e07abcd9614712
ALI AHMED <prgmr.aahmed@gmail.com>
2026-05-10 19:09:44 +0500

    Enhance Student class with grades management
    Added a grades member and addGrade method to Student.

--- LMS_CCP/Student.h
+++ LMS_CCP/Student.h
@@ -1,16 +1,18 @@
 #pragma once
 #include "User.h"
-
+#include "DynamicArray"
 class Course;
 
 class Student : public User {
 private:
     float cgpa;
+    DynamicArray<double> grades;
 public:
     Student(const std::string& n, int i, float c);
     void displayDashboard() const override;
     void setCGPA(float n);
     float getCGPA() const;
+    void addGrade(double score); 
     void comment(Course* c, const std::string& msg, IObserver* sender);
     void comment(Course* c, const std::string& msg);
 };
